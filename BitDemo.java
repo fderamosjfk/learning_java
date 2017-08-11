1541860311cf10dcdd9777a621d0ab67ba28993e
Ferdinand DeRamos <jfk105@gmail.com>
2017-08-11 02:04:44 -0400

    more java practice programs

---
+++ BitDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Uses the bitwise AND operator to print the number "2" to standard output.
+ */
+
+public class BitDemo {
+    public static void main(String[] args) {
+        int bitmask = 0x000F;
+        int val = 0x2222;
+        // prints "2"
+        System.out.println(val & bitmask);
+    }
+}

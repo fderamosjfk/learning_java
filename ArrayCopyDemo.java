e86b85aa056f4c7f0adc1a2417f1214b950a664a
Ferdinand DeRamos <jfk105@gmail.com>
2017-08-11 01:31:48 -0400

    new sample java programs

---
+++ ArrayCopyDemo.java
@@ -0,0 +1,15 @@
+/**
+ * Declare an array of char elements, spelling the word "decaffeinated." 
+ * It uses the System.arraycopy method to copy a subsequence of array components into a second array.
+ */
+
+public class ArrayCopyDemo {
+    public static void main(String[] args) {
+        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
+			    'i', 'n', 'a', 't', 'e', 'd' };
+        char[] copyTo = new char[7];
+
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        System.out.println(new String(copyTo));
+    }
+}
---
+++ MultiDimArrayDemo.java
@@ -0,0 +1,17 @@
+/**
+ * A Java multidimensional array is an array whose components are themselves arrays.
+ * The rows are allowed to vary in length.
+ */
+
+public class MultiDimArrayDemo {
+    public static void main(String[] args) {
+        String[][] names = {
+            {"Mr. ", "Mrs. ", "Ms. "},
+            {"Smith", "Jones"}
+        };
+        // Mr. Smith
+        System.out.println(names[0][0] + names[1][0]);
+        // Ms. Jones
+        System.out.println(names[0][2] + names[1][1]);
+    }
+}

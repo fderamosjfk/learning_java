df1aaf2bc764264b260fe5552ed466f06f690100
Ferdinand DeRamos <jfk105@gmail.com>
2017-08-11 02:18:49 -0400

    new java practice program on another git branch

---
+++ IfElseDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Assigns a grade based on the value of a test score:
+ *  an A for a score of 90% or above, a B for a score of 80% or above, and so on.
+ */
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+
+        int testscore = 76;
+        char grade;
+
+        if (testscore >= 90) {
+            grade = 'A';
+        } else if (testscore >= 80) {
+            grade = 'B';
+        } else if (testscore >= 70) {
+            grade = 'C';
+        } else if (testscore >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("Grade = " + grade);
+    }
+}

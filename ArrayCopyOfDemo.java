426659395ff3b6d2a9d49e62cd9a1c59942bc5fd
Ferdinand DeRamos <jfk105@gmail.com>
2017-08-11 02:00:00 -0400

    more java practice programs

---
+++ ArrayCopyOfDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Using the copyOfRange method does not require you to create the destination array
+ * before calling the method, because the destination array is returned by the method.
+ */
+
+public class ArrayCopyOfDemo {
+    public static void main(String[] args) {
+        
+        char[] copyFrom = {'d', 'e', 'c', 'a', 'f', 'f', 'e',
+            'i', 'n', 'a', 't', 'e', 'd'};
+            
+        char[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+        
+        System.out.println(new String(copyTo));
+    }
+}
---
+++ ConcatDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Concatenating (joining) two strings together.
+ */
+
+public class ConcatDemo {
+    public static void main(String[] args){
+        String firstString = "This is";
+        String secondString = " a concatenated string.";
+        String thirdString = firstString+secondString;
+        System.out.println(thirdString);
+    }
+}

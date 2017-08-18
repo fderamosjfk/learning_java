e0deb3da082049b44cd151617feee4aca6996da4
Ferdinand DeRamos <jfk105@gmail.com>
2017-08-17 22:45:52 -0400

    EasyMock is an awesome Mocking Framework

---
+++ EASYMOCK/MathApplicationStrictMockTest.java
@@ -0,0 +1,44 @@
+import org.easymock.EasyMock;
+import org.easymock.EasyMockRunner;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(EasyMockRunner.class)
+public class MathApplicationStrictMockTest {
+	
+   private MathApplication mathApplication;
+   private CalculatorService calcService;
+   
+   @Before
+   public void setUp(){
+      mathApplication = new MathApplication();
+      calcService = EasyMock.createStrictMock(CalculatorService.class);
+      mathApplication.setCalculatorService(calcService);
+   }
+
+   @Test
+   public void testAddAndSubtract(){
+      
+      //add the behavior to add numbers
+      EasyMock.expect(calcService.add(20.0,10.0)).andReturn(30.0);
+      
+      //subtract the behavior to subtract numbers
+      EasyMock.expect(calcService.subtract(20.0,10.0)).andReturn(10.0);
+      
+      //activate the mock
+      EasyMock.replay(calcService);	
+	
+      //test the subtract functionality
+      // calling subtract before add will fail the test on strict mock
+      Assert.assertEquals(mathApplication.subtract(20.0, 10.0),10.0,0);
+      
+      //test the add functionality
+      Assert.assertEquals(mathApplication.add(20.0, 10.0),30.0,0);
+      
+      //verify call to calcService is made or not
+      EasyMock.verify(calcService);
+   }
+}

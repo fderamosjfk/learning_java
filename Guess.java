8508025a289057f3b5a4c4f45b8f67c3f1f0da64
Ferdinand DeRamos <jfk105@gmail.com>
2017-08-18 23:42:25 -0400

    simple example

--- Guess.java
+++ Guess.java
@@ -1,20 +1,31 @@
 // Guess the letter game.  
 
 class Guess {   
+  public static void main(String args[])
+    throws java.io.IOException {
 
-  public static void main(String args[])   
-    throws java.io.IOException { 
+    char ch, ignore, answer = 'K';
 
-    char ch, answer = 'K'; 
+    do {
+      System.out.println("I'm thinking of a letter between A and Z.");
+      System.out.print("Can you guess it: ");
 
- 
-    System.out.println("I'm thinking of a letter between A and Z."); 
-    System.out.print("Can you guess it: "); 
+      // read a character
+      ch = (char) System.in.read();
 
-    ch = (char) System.in.read(); // read a char from the keyboard 
+      // discard any other characters in the input buffer
+      do {
+        ignore = (char) System.in.read();
+      } while(ignore != '\n');
 
-    if(ch == answer) System.out.println("** Right **"); 
+      if(ch == answer) System.out.println("** Right **");
+      else {
+        System.out.print("...Sorry, you're ");
+        if(ch < answer) System.out.println("too low");
+        else System.out.println("too high");
+        System.out.println("Try again!\n");
+      }
+    } while(answer != ch);
 
-  }   
-
+  }
 }
